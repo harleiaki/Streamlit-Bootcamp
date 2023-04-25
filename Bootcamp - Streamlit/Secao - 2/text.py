12047862cfcb6609b0336fc4dccf83dd58805078
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-04-25 18:48:34 -0300

    Add files via upload

---
+++ Bootcamp - Streamlit/Secao - 2/text.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("I am Title")
+
+st.header("I am Header")
+
+st.subheader("I am Sub Header")
+
+st.markdown("I am Markdown")
+
+st.code("I am a code block. Eg - int a = 5")
+
+st.text("I am Text")
+
+st.write("I am write")
